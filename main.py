dbf6e145c3d78ddb198b6b869c61d3ab24b76b6c
Evgeniy Acteck <makdak23@mail.ru>
2022-10-25 23:18:48 +0300

    0.0.2 fix errors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Low_level_static_array:
-    def __init__(self, size: int):  # выделим память под статический массив
+    def __init__(self, size: int):  # выделение памяти под статический массив
         self.memory = []
         for _ in range(size):
             self.memory.append(None)
@@ -10,22 +10,18 @@ class Low_level_static_array:
 
     def append(self, value, index=0):  # вставка (или добавление в конец) элемента на позицию
         ind = self.get_fill_len()
-        if index < 0 or index > len(self.memory):  # вне разрешенного диапазона
-            raise IndexError
         if ind + 1 < index or index == 0:
             index = ind + 1
         while ind >= index:
             self.memory[ind] = self.memory[ind - 1]
             ind -= 1
         self.memory[ind] = value
-        print(self.memory, f" --- '{value}' добавлен на позицию № {index}")
+        print(self.memory, f'элемент {value} добавлен на позицию {index}')
 
         return self.memory
 
     def delete(self, index=0):  # удаление по индексу (или удаление последнего)
         ind = self.get_fill_len()
-        if index > ind or index < 0 or ind < 1:
-            raise IndexError
         if ind + 1 < index or index == 0:
             index = ind + 1
         while ind > index:
@@ -33,22 +29,18 @@ class Low_level_static_array:
             index += 1
         value = self.memory[ind - 1]
         self.memory[ind - 1] = None
-        print(self.memory, f" --- '{value}' удалён с позиции № {ind}")
+        print(self.memory, f'элемент {value} удалён с позиции {ind}')
         return value
 
     def get_elem(self, index):  # получение элемента по индексу
         ind = self.get_fill_len()
-        if index > ind or index < 1:
-            raise IndexError
         print(f"{index}-й элемент равен {self.memory[index - 1]!r}")
         return self.memory[index - 1]
 
     def set_elem(self, index, value):  # изменение значения элемента на новое по его индексу
         ind = self.get_fill_len()
-        if index > ind or index < 1:
-            raise IndexError
         self.memory[index - 1] = value
-        print(self.memory, f" --- элемент № {index} изменен на '{value}'")
+        print(self.memory, f'элемент {index} изменен на {value}')
 
     def __str__(self):
         return self.memory
@@ -56,12 +48,15 @@ class Low_level_static_array:
 
 def main():
     # демонстрация работы
-    arr = Low_level_static_array(3)
+    arr = Low_level_static_array(5)
     arr.append("1")
     arr.append("2")
+    arr.append("3")
     arr.append("4", 2)
+    arr.set_elem(3, "8")
     arr.get_elem(2)
     arr.delete(3)
+    arr.delete()
     arr.delete()
     arr.delete()
 
